3f94a55713aaae12555b1e5a764dfc4a7a5c2c50
pinir12 <pinchosroth@gmail.com>
2025-08-04 10:15:19 +0100

    IP address ui

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import { useState, useEffect } from 'react';
 
 function HomePage() {
-  const [ipAddress, setIpAddress] = useState('0.0.0.0');
+  const [ipAddress, setIpAddress] = useState('');
    const [dateTime, setDateTime] = useState(new Date());
 
   useEffect(() => {
@@ -32,7 +32,9 @@ function HomePage() {
       </Head>
       <h1 id="time" className="text-5xl font-bold mb-8"> {dateTime.toLocaleString()}</h1>
       <h3 className="text-3xl font-bold mb-8">&#x1F7E2; All systems operational</h3>
-      <p id="ip-text" className="text-lg mb-2">The originating IP address of the client-side request is {ipAddress}</p>
+      <p id="ip-text" className="text-lg mb-2">The originating IP address of the client-side request is 
+        {ipAddress? <span> {ipAddress}</span>: <span className='text-gray-500'> 0.0.0.0</span>}
+        </p>
       <p className="text-lg mt-4 text-gray-900">This endpoint operates as a verification mechanism for assessing configuration integrity and validating protocol-layer connectivity.</p>
     </div>
   );
