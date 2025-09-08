b285a918281cc0b4cb3a332666931c58eaeea562
pinir12 <pinchosroth@gmail.com>
2025-09-08 11:01:51 +0100

    Fixed error for admin user

--- pages/api/data.js
+++ pages/api/data.js
@@ -206,7 +206,7 @@ export default async function handler(req, res) {
             res.status(200).json({
                 title: videoTitle,
                 url: videoDirectUrl,
-                rowId: rowId[0].id,
+                rowId: rowId[0].id || '',
             });
 
         } catch (error) {
